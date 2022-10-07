18825db5d46bb7043480edb9a573be34b3e95c9a
krayf <freestonekray@gmail.com>
2022-10-07 16:56:35 -0400

    reorganize data endpoint

--- server/plumber.R
+++ server/plumber.R
@@ -4,6 +4,7 @@ library(assertthat)
 library(sf)
 library(geojsonsf)
 library(dbplyr)
+library(glue)
 
 # custom function imports
 source(here('modules', 'helper.R'))
@@ -11,7 +12,7 @@ source(here('modules', 'variables.R'))
 source(here('modules', 'geom.R'))
 
 con <- get_con()
-print('Got con!')
+
 #* @apiTitle census-find api
 #* @apiDescription Helps power the front-end of census-find. Mainly returns data from the database
 
@@ -21,18 +22,7 @@ print('Got con!')
 #* @param shallow can be true or false
 #* @get /variables
 function(type, year, shallow) {
-  # make sure all parameters are acceptable
-  type_assert <- assert_help(type, c('acs5', 'pl', 'sf1'))
-  year_assert <- assert_help(year, c('2010', '2020'))
-  shallow_assert <- assert_help(shallow, c('TRUE','FALSE'))
-  valid <- all(c(type_assert, year_assert, shallow_assert))
-  if (valid) {
-    print(glue('Returning dataset {type}_{year}_vars'))
-    return (variables_get(con, year, type, shallow))
-  } else {
-    stop()
-    return ()
-  }
+  variables_get(con, year, type, shallow)
 }
 
 #* Only meant to display geometries for the front-end map
@@ -45,40 +35,6 @@ function(type, state=NULL) {
 #* Only meant to display geometries for the front-end map
 #* @post /data
 function(req) {
-  body <- req$body
-  body_args <- names(body)
-
-  # TODO determine the data structure, b/c maybe it should be something like:
-  # {
-  #   "acs5": {
-  #     "year": 2020,
-  #     "vars": []
-  #   },
-  #   "sf1": {
-  #     "year": 2010,
-  #     "vars": []
-  #   }
-  # }
-  # get the arguments
-  dataset <- body$dataset
-  year <- body$year
-  geoid <- body$geoid
-  geom <- body$geom
-
-  # assert the proper arguments are passed in
-  dataset_assert <- assert_help(body$dataset, c('acs5', 'pl', 'sf1'))
-  year_assert <- assert_help(body$year, c('2010', '2020'))
-  geom_assert <- assert_help(geom, c('states', 'tracts', 'counties', 'places'))
-
-  # TODO assert variables are valid
-  query <- glue('SELECT name FROM {dataset}_{year}_vars;')
-  all_names <- dbGetQuery(con, query)
-
-  # then assert the values of those arguments are valid
-  if (!(all(geom_assert, year_assert, dataset_assert))) {
-    stop('Invalid Parameters')
-  }
-  
-  return ('yo')
+  data_post(con, req)
 }
 
