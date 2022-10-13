85ef6a2af43bd4de15c9b3986221be6462b850e8
krayf <freestonekray@gmail.com>
2022-10-13 08:21:08 -0400

    search for geography screen started

--- src/components/Explore/Explore.tsx
+++ src/components/Explore/Explore.tsx
@@ -1,12 +1,127 @@
-import React, { FC } from 'react'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { Text, Stack, Center, Button, Box, Heading, Link, Avatar, Input, Table, TableCaption, TableContainer, Tbody, Td, Tfoot, Th, Thead, Tr } from '@chakra-ui/react'
+import React, { FC, useEffect } from 'react'
 import styles from './Explore.module.scss'
 
 // interface ExploreProps {}
 
-const Explore: FC<any> = () => (
-  <div className={styles.Explore} data-testid="Explore">
-    Explore Component
-  </div>
-)
+const Explore: FC<any> = () => {
+  useEffect(() => {
+  }, [])
+  const handleChange = (event: any): void => {
+    console.log(event.target.value)
+  }
+  return (
+    <div className={styles.Explore} data-testid="Explore">
+
+      <Center py={6}>
+        <Box
+          maxW={'320px'}
+          w={'full'}
+          // bg={useColorModeValue('white', 'gray.900')}
+          boxShadow={'2xl'}
+          rounded={'lg'}
+          p={6}
+          textAlign={'center'}>
+
+          <Heading fontSize={'2xl'} fontFamily={'body'}>
+            Search for a Place
+          </Heading>
+          <Text fontWeight={600} color={'gray.500'} mb={4}>
+            e.g. state, county, census tract, place
+          </Text>
+          <Input
+            // value={value}
+            onChange={handleChange}
+            variant='filled'
+            placeholder='Search'
+            size='md'
+          />
+          <Stack mt={8} direction={'row'} spacing={4}>
+            {/* <Button
+              flex={1}
+              fontSize={'sm'}
+              rounded={'full'}
+              _focus={{
+                bg: 'gray.200'
+              }}>
+              Message
+            </Button> */}
+            <Button
+              flex={1}
+              fontSize={'md'}
+              rounded={'full'}
+              bg={'blue.400'}
+              color={'white'}
+              boxShadow={
+                '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+              }
+              _hover={{
+                bg: 'blue.500'
+              }}
+              _focus={{
+                bg: 'blue.500'
+              }}>
+              Search
+            </Button>
+          </Stack>
+        </Box>
+
+      </Center>
+      {/* table results */}
+      <Center>
+
+      <Box
+        maxW={'500px'}
+        w={'full'}
+        // bg={useColorModeValue('white', 'gray.900')}
+        boxShadow={'2xl'}
+        rounded={'lg'}
+        p={6}
+        textAlign={'center'}>
+
+        <TableContainer>
+          <Table variant='striped' colorScheme='gray'>
+            <TableCaption>Imperial to metric conversion factors</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>Geography</Th>
+                <Th>Type</Th>
+                <Th isNumeric>multiply by</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>inches</Td>
+                <Td>millimetres (mm)</Td>
+                <Td isNumeric>25.4</Td>
+              </Tr>
+              <Tr>
+                <Td>feet</Td>
+                <Td>centimetres (cm)</Td>
+                <Td isNumeric>30.48</Td>
+              </Tr>
+              <Tr>
+                <Td>yards</Td>
+                <Td>metres (m)</Td>
+                <Td isNumeric>0.91444</Td>
+              </Tr>
+            </Tbody>
+            <Tfoot>
+              <Tr>
+                <Th>To convert</Th>
+                <Th>into</Th>
+                <Th isNumeric>multiply by</Th>
+              </Tr>
+            </Tfoot>
+          </Table>
+        </TableContainer>
+      </Box>
+      </Center>
+
+    </div>
+  )
+}
 
 export default Explore
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,7 +14,7 @@ import {
   IconButton,
   HStack
 } from '@chakra-ui/react'
-import { FaBars, FaWindowClose } from 'react-icons/fa'
+import { FaBars, FaQuestionCircle, FaWindowClose } from 'react-icons/fa'
 import { ColorModeSwitcher } from '../../ColorModeSwitcher'
 
 // interface NavbarProps {}
@@ -63,40 +63,27 @@ const Navbar: FC<any> = () => {
           </HStack>
           <Flex alignItems={'center'}>
             <ColorModeSwitcher justifySelf='flex-end' />
+            <IconButton
+              size={'md'}
+              icon={<Center><FaQuestionCircle /></Center>}
+              aria-label={'About Page'}
+              color="current"
+              variant="ghost"
+              onClick={() => alert('This about page does not exist...yet')}
+            />
 
-            {/* <Menu>
-              <MenuButton
-                as={Button}
-                rounded={'full'}
-                variant={'link'}
-                cursor={'pointer'}
-                minW={0}>
-                <Avatar
-                  size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
-                />
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Link 1</MenuItem>
-                <MenuItem>Link 2</MenuItem>
-                <MenuDivider />
-                <MenuItem>Link 3</MenuItem>
-              </MenuList>
-            </Menu> */}
           </Flex>
         </Flex>
 
         {isOpen
           ? (
-          <Box pb={4} display={{ md: 'none' }}>
-            <Stack as={'nav'} spacing={4}>
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
-              ))}
-            </Stack>
-          </Box>
+            <Box pb={4} display={{ md: 'none' }}>
+              <Stack as={'nav'} spacing={4}>
+                {Links.map((link) => (
+                  <NavLink key={link}>{link}</NavLink>
+                ))}
+              </Stack>
+            </Box>
             )
           : null}
       </Box>
